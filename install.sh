04d87c5adc7b1cdd660492a062137e2a1cd80d41
sasas <aldiyardaniyal@gmail.com>
2026-03-06 12:18:39 +0500

    rewrote install script

---
+++ install.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo "== Fedora Zsh Setup =="
+
+# 1. Установка зависимостей
+echo "Устанавливаю пакеты..."
+sudo dnf install -y zsh git curl util-linux-user
+
+# 2. Установка Oh My Zsh
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+    echo "Устанавливаю Oh My Zsh..."
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+fi
+
+# 3. Установка плагина autosuggestions
+ZSH_CUSTOM="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"
+
+if [ ! -d "$ZSH_CUSTOM/plugins/zsh-autosuggestions" ]; then
+    echo "Устанавливаю zsh-autosuggestions..."
+    git clone https://github.com/zsh-users/zsh-autosuggestions \
+        $ZSH_CUSTOM/plugins/zsh-autosuggestions
+fi
+
+# 4. Копирование .zshrc
+if [ -f ".zshrc" ]; then
+    echo "Применяю настройки .zshrc..."
+    cp .zshrc "$HOME/.zshrc"
+fi
+
+# 5. Сделать zsh shell по умолчанию
+if [ "$SHELL" != "$(which zsh)" ]; then
+    echo "Меняю shell на zsh..."
+    chsh -s $(which zsh)
+fi
+
+echo "Готово! Перезапустите терминал или выполните:"
+echo "exec zsh"
